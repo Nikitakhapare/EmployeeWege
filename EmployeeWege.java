d83758b3b160c5737354d4ebf19a50c9b86707fc
Nikita <nikitakhapare53@gmail.com>
2022-02-02 08:15:19 +0530

    Updaed Indentation

--- EmployeeWege.java
+++ EmployeeWege.java
@@ -3,21 +3,21 @@ package EmployeeWeag;
 public class EmployeeWege {
 	public static void main(String[] args) {
 		//constant
-		int Is_Full_Time=1;
-		int Rate_Per_Hour=20;
-		int Is_Part_Time=0;
+		int isFullTime=1;
+		int ratePerHour=20;
+		int isPartTime=0;
 		int empHour=0;
 		int empWege=0;
 		//computation
 		double empCheck= Math.floor(Math.random()*10)%3;
 		//condition
-		if(empCheck==Is_Full_Time)
+		if(empCheck==isFullTime)
 		    empHour=8;
-		else if(empCheck==Is_Part_Time)
+		else if(empCheck==isPartTime)
 			empHour=4;
 		else
 			empHour=0;
-		empWege=empHour*Rate_Per_Hour;
+		empWege=empHour*ratePerHour;
 		System.out.println("Employee Wege= "+empWege);
 		}
 
