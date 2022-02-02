09f56e1313b190d1ca2c1d0148acc21b5d0e162c
Nikita <nikitakhapare53@gmail.com>
2022-02-02 08:05:25 +0530

    Updated Indentation

--- EmpWegeCond.java
+++ EmpWegeCond.java
@@ -4,30 +4,30 @@ public class EmpWegeCond {
 
          public static void main(String[] args) {
 	        
-	        int Is_Present = 1, monthly_wage = 0, total_hour = 0; 
-	        int day_present = 0; int Is_Full_Time=1; int empHour; int Is_Part_Time=0;
-            Double EmpCheck = Math.floor(Math.random() * 10) % 3;
-            if(EmpCheck==Is_Full_Time)
+	        int isPresent = 1, monthlyWage = 0, totalHour = 0; 
+	        int dayPresent = 0; int isFullTime=1; int empHour; int isPartTime=0;
+                Double empCheck = Math.floor(Math.random() * 10) % 3;
+                if(empCheck==isFullTime)
             	empHour=8;
-            else if(EmpCheck==Is_Part_Time)
+                else if(empCheck==isPartTime)
             	empHour=4;
             else
             	empHour=0;
             	
 	        for(int i=1;i<=31;i++) {
-	            Double EmpStatus = Math.floor(Math.random() * 10) % 2;
-	            if (EmpStatus == Is_Present) {
-	                if (day_present < 20 && total_hour < 100) {
-	                    day_present += 1;
-	                    total_hour += empHour;
+	            Double empStatus = Math.floor(Math.random() * 10) % 2;
+	            if (empStatus == isPresent) {
+	                if (dayPresent < 20 && totalHour < 100) {
+	                    dayPresent += 1;
+	                    totalHour += empHour;
 	                }
 
 	            }
 	        }
-	        System.out.println("working day "+day_present);
-	        System.out.println("total hour worked "+total_hour);
-	        monthly_wage = total_hour * 20;
-	        System.out.println("monthly salary is "+monthly_wage);
+	        System.out.println("working day "+dayPresent);
+	        System.out.println("total hour worked "+totalHour);
+	        monthlyWage = totalHour * 20;
+	        System.out.println("monthly salary is "+monthlyWage);
 	    }
 	}
 	
