16d4cbbce70677af5c2f72c179ecf70139a600c5
Nikita <nikitakhapare53@gmail.com>
2022-02-02 08:12:42 +0530

    Updated Variable Declaration

--- EmpWegeSwitch.java
+++ EmpWegeSwitch.java
@@ -2,25 +2,27 @@ package EmployeeWeag;
 
 public class EmpWegeSwitch {
 	//class constants for case
-	public static final int Is_Full_Time=1;
-	public static final int Rate_Per_Hour=20;
-	public static final int Is_Part_Time=0;
-    public static void main(String[] args) {
+	public static final int IsFullTime=1;
+	public static final int RatePerHour=20;
+	public static final int IsPartTime=0;
+        public static void main(String[] args)
+       {
 		//variables
     	int empHour=0;
-		int empWege=0;
+	int empWege=0;
 		//computation
-		double empCheck= Math.floor(Math.random()*10)%3;
-		switch ((int) empCheck) {
-	    case Is_Full_Time : empHour=8;
+	double empCheck= Math.floor(Math.random()*10)%3;
+	switch ((int) empCheck) 
+        {
+	   case isFullTime : empHour=8;
 		                    break;
-	    case Is_Part_Time : empHour=4;
+           case isPartTime : empHour=4;
 		                   break;
 		          default : empHour=0;
 		                    
-		}
+        }
 			
-		empWege=empHour*Rate_Per_Hour;
+		empWege=empHour*RatePerHour;
 		System.out.println("Employee Wege= "+empWege);
 
 
