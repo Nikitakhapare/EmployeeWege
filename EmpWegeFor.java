afc8fae7a3b017adbe1cff2ac1308cf9e792dc6b
Nikita <nikitakhapare53@gmail.com>
2022-02-02 08:09:21 +0530

    Updated Indentation

--- EmpWegeFor.java
+++ EmpWegeFor.java
@@ -3,28 +3,30 @@ package EmployeeWeag;
 public class EmpWegeFor{
 	public static void main(String[] args) { 
 	        //constant
-			int Is_Full_Time=1;
-			int Rate_Per_Hour=20;
-			int Is_Part_Time=0;
+			int isFullTime=1;
+			int ratePerHour=20;
+			int isPartTime=0;
 			int empHour=0;
 			int empWege=0;
-			int Total_empWege=0;
+			int totalEmpWege=0;
 			
 			//computation
 			for(int day=1;day<=20;day++) {
 			double empCheck= Math.floor(Math.random()*10)%3;
 			//condition
-			if(empCheck==Is_Full_Time)
+			if(empCheck==isFullTime)
 			    empHour=8;
-			else if(empCheck==Is_Part_Time)
+
+			else if(empCheck==isPartTime)
 				empHour=4;
 			else
 				empHour=0;
-			empWege=empHour*Rate_Per_Hour;
-			Total_empWege+=empWege;
+			
+                        empWege=empHour*ratePerHour;
+			totalEmpWege+=empWege;
 			
 			}
-			System.out.println("Employee Wege for month= "+Total_empWege);
+			System.out.println("Employee Wege for month= "+totalEmpWege);
 
 }
 }
